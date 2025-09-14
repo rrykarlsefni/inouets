d9e4d149ddd80e416eae69708541238c3b45f7c9
rrykarlsefni <rrykarlsefni@gmail.com>
2025-09-14 09:35:05 +0700

    aku

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 msg=${1:-update}
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$current_branch" != "master" ]; then
+  exit 1
+fi
+git pull origin master --rebase
 git add .
 git commit -m "$msg"
 git push origin master
